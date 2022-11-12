71c9f8556bf87c9976a4b4235d59535a695c7d2e
Dominik_Kurasbediani <dominik_k@cherrypickgames.com>
2022-11-12 16:58:58 +0100

    Added basic window and loop

---
+++ GameProgrammingOverview/Game.cpp
@@ -0,0 +1,82 @@
+﻿#include <SDL.h>
+#include "Game.h"
+
+Game::Game()
+{
+    mWindow = nullptr;
+    mIsRunning = true;
+}
+
+bool Game::Initialize()
+{
+    int sdlResult = SDL_Init(SDL_INIT_VIDEO);
+    if (sdlResult != 0)
+    {
+        SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+        return false;
+    }
+
+    mWindow = SDL_CreateWindow("Chapter 1", 100, 100, 800, 600, 0);
+    if (!mWindow)
+    {
+        SDL_Log("Unable to initialize window: %s", SDL_GetError());
+        return false;
+    }
+
+    mRenderer = SDL_CreateRenderer(mWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if(!mRenderer)
+    {
+        SDL_Log("Unable to initialize renderer: %s", SDL_GetError());
+        return false;
+    }
+    return true;
+}
+
+void Game::Update()
+{
+    while (mIsRunning)
+    {
+        ProcessInput();
+        UpdateGame();
+        GenerateOutput();
+    }
+}
+
+void Game::Shutdown()
+{
+    SDL_DestroyWindow(mWindow);
+    SDL_DestroyRenderer(mRenderer);
+    SDL_Quit();
+}
+
+void Game::ProcessInput()
+{
+    SDL_Event event;
+
+    // returns true if there are events in the q
+    while (SDL_PollEvent(&event))
+    {
+        switch (event.type)
+        {
+        case SDL_QUIT:
+            mIsRunning = false;
+            break;
+        }
+
+        const Uint8* state = SDL_GetKeyboardState(nullptr);
+        if(state[SDL_SCANCODE_ESCAPE])
+            mIsRunning = false;
+        
+    }
+}
+
+void Game::UpdateGame()
+{
+}
+
+void Game::GenerateOutput()
+{
+    SDL_SetRenderDrawColor(mRenderer, 0, 0, 0, 255);
+    SDL_RenderClear(mRenderer);
+    SDL_RenderPresent(mRenderer);
+}
---
+++ GameProgrammingOverview/Game.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+class SDL_Window;
+class SDL_Renderer;
+class Game
+{
+public:
+    Game();
+
+    bool Initialize();
+    void Update();
+    void Shutdown();
+
+private:
+    void ProcessInput();
+    void UpdateGame();
+    void GenerateOutput();
+
+    SDL_Window* mWindow;
+    SDL_Renderer* mRenderer;
+    bool mIsRunning;
+    
+};
