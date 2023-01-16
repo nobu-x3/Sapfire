d627fb69632acb16239c594b7b834bfa0add83a2
Dominik Kurasbediani <dominik.kurasbediani@gmail.com>
2023-01-17 00:04:12 +0100

    Removed refs to libraries that could not be compiled with clang

--- Project/src/engine/TileMapComponent.cpp
+++ Project/src/engine/TileMapComponent.cpp
@@ -1,49 +1,49 @@
 #include "TileMapComponent.h"
-#include "csv.hpp"
-#include "internal/csv_row.hpp"
+// #include "csv.hpp"
+// #include "internal/csv_row.hpp"
 #include <SDL_log.h>
 #include <SDL_render.h>
 
 void TileMapComponent::ReadCSV(std::string path, int height, int width)
 {
-	mHeight = mTexHeight / height;
-	mWidth = mTexWidth / width;
-	csv::CSVReader reader(path); // lmao this library does not reset iterators or smth
-	int destOffsetY = 0;
-	for (csv::CSVRow &row : reader)
-	{
-		int destOffsetX = 0;
-		for (csv::CSVField &field : row)
-		{
-			int index = std::stoi(field.get());
+	// mHeight = mTexHeight / height;
+	// mWidth = mTexWidth / width;
+	// csv::CSVReader reader(path); // lmao this library does not reset iterators or smth
+	// int destOffsetY = 0;
+	// for (csv::CSVRow &row : reader)
+	// {
+	// 	int destOffsetX = 0;
+	// 	for (csv::CSVField &field : row)
+	// 	{
+	// 		int index = std::stoi(field.get());
 
-			int temp = 0;
-			int offsetY = index / mWidth;
-			int offsetX = index - (offsetY * mWidth);
+	// 		int temp = 0;
+	// 		int offsetY = index / mWidth;
+	// 		int offsetX = index - (offsetY * mWidth);
 
-			SDL_Rect srcRect;
-			srcRect.x = offsetX * width;
-			srcRect.y = offsetY * height;
-			srcRect.w = width;
-			srcRect.h = height;
+	// 		SDL_Rect srcRect;
+	// 		srcRect.x = offsetX * width;
+	// 		srcRect.y = offsetY * height;
+	// 		srcRect.w = width;
+	// 		srcRect.h = height;
 
-			SDL_Rect destRect;
-			destRect.x = destOffsetX * width * mOwner->GetScale();
-			destRect.y = destOffsetY * height * mOwner->GetScale();
-			destRect.h = mOwner->GetScale() * height;
-			destRect.w = mOwner->GetScale() * width;
+	// 		SDL_Rect destRect;
+	// 		destRect.x = destOffsetX * width * mOwner->GetScale();
+	// 		destRect.y = destOffsetY * height * mOwner->GetScale();
+	// 		destRect.h = mOwner->GetScale() * height;
+	// 		destRect.w = mOwner->GetScale() * width;
 
-			Tile tile;
-			tile.destrect = destRect;
-			tile.srcrect = srcRect;
-			mTiles.emplace_back(tile);
+	// 		Tile tile;
+	// 		tile.destrect = destRect;
+	// 		tile.srcrect = srcRect;
+	// 		mTiles.emplace_back(tile);
 
-			destOffsetX++;
-			SDL_Log("srcX - %d, srcY - %d, destX - %d, destY - %d", srcRect.x, srcRect.y, destRect.x,
-				destRect.y);
-		}
-		destOffsetY++;
-	}
+	// 		destOffsetX++;
+	// 		SDL_Log("srcX - %d, srcY - %d, destX - %d, destY - %d", srcRect.x, srcRect.y, destRect.x,
+	// 			destRect.y);
+	// 	}
+	// 	destOffsetY++;
+	// }
 }
 
 /* void TileMapComponent::Draw(SDL_Renderer *renderer) */
