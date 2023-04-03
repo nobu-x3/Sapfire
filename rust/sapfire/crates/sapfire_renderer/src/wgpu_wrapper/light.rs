7560cb2a472a3d65e0c21da712bb595cd1a05460
Dominik <dominik.kurasbediani@gmail.com>
2023-04-03 23:53:49 +0200

    Removed rust stuff

--- rust/sapfire/crates/sapfire_renderer/src/wgpu_wrapper/light.rs
+++
@@ -1,6 +0,0 @@
-#[repr(C)]
-#[derive(Debug, Copy, Clone, bytemuck::Pod, bytemuck::Zeroable)]
-pub struct LightUniform {
-    pub position: [f32; 4], // 4th element is padding
-    pub color: [f32; 4],
-}
--- rust/sapfire/src/main.rs
+++
@@ -1,5 +0,0 @@
-use sapfire_renderer::*;
-fn main() {
-    let res = pollster::block_on(SapfireRenderer::new());
-    SapfireRenderer::run(res);
-}
