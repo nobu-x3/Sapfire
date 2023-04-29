939b411a34ce36a8a5e4699c176d587cc09d7a81
Dominik <dominik.kurasbediani@gmail.com>
2023-04-30 00:29:58 +0200

    Removed accidentally commited bins

--- build/sandbox/CMakeFiles/sandbox.dir/src/main.c.o.d
+++
@@ -1,4 +0,0 @@
-sandbox/CMakeFiles/sandbox.dir/src/main.c.o: \
-  /home/nobu/projects/Sapfire/sandbox/src/main.c \
-  /home/nobu/projects/Sapfire/sapfire/src/test.h \
-  /home/nobu/projects/Sapfire/sapfire/src/defines.h
