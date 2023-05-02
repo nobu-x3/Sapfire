46b9a8ba2bd0b4c7b837d19ff63ad817747c1636
Dominik <dominik.kurasbediani@gmail.com>
2023-05-02 20:21:12 +0200

    Scaffolded vulkan instance creation

--- sapfire/src/renderer/vulkan/vulkan_provider.c
+++ sapfire/src/renderer/vulkan/vulkan_provider.c
@@ -1,7 +1,36 @@
+#include "core/logger.h"
 #include "vulkan_provider.h"
+#include "vulkan_types.h"
+#include <vulkan/vulkan_core.h>
+
+// TODO: really think about singletons...
+static vulkan_context context;
 
 b8 vulkan_initialize(renderer_provider *api, const char *app_name,
 					 struct platform_state *plat_state) {
+		// TODO: implement custom allocators.
+		context.allocator = SF_NULL;
+		VkApplicationInfo app_info = {VK_STRUCTURE_TYPE_APPLICATION_INFO};
+		app_info.apiVersion = VK_API_VERSION_1_3;
+		app_info.pApplicationName = app_name;
+		app_info.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+		app_info.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+		app_info.pEngineName = "Sapfire Engine";
+
+		VkInstanceCreateInfo create_info = {
+			VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO};
+		create_info.pApplicationInfo = &app_info;
+		create_info.enabledExtensionCount = 0;
+		create_info.ppEnabledExtensionNames = SF_NULL;
+		create_info.enabledLayerCount = 0;
+		create_info.ppEnabledLayerNames = SF_NULL;
+
+		VkResult result = vkCreateInstance(&create_info, context.allocator,
+										   &context.instance);
+		if (result != VK_SUCCESS) {
+				SF_FATAL("Failed to create VkInstance.");
+				return FALSE;
+		}
 		return TRUE;
 }
 
--- sapfire/src/renderer/vulkan/vulkan_types.h
+++ sapfire/src/renderer/vulkan/vulkan_types.h
@@ -2,3 +2,8 @@
 
 #include "defines.h"
 #include <vulkan/vulkan.h>
+
+typedef struct vulkan_context{
+	VkInstance instance;
+	VkAllocationCallbacks* allocator;
+} vulkan_context;
