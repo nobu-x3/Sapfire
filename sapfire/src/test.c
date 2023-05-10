d6efc6943b0793ad8cf0ae42c8aa12d5a5eed920
Dominik <dominik.kurasbediani@gmail.com>
2023-05-10 22:15:26 +0200

    Removed old test scripts

--- sapfire/src/test.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-#include "core/asserts.h"
-#include "core/logger.h"
-#include "test.h"
-
-void print_int(i32 num) { printf("Num is %i\n", num); }
--- sapfire/src/test.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include "defines.h"
-
-SAPI void print_int(i32 i);
