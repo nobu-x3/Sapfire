94ec534ce313db36230163b83a00c3f09cfa3992
Dominik <dominik.kurasbediani@gmail.com>
2023-05-17 12:01:46 +0200

    Fixed windows builds

--- build-cmake.sh
+++ build-cmake.sh
@@ -5,7 +5,7 @@ mkdir -p build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=.. -DBUILD_SHARED_LIBS=ON -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 make
-cd ..
-./compile_shaders.sh
-cd build
+# cd ..
+# ./compile_shaders.sh
+# cd build
 ./sandbox/sandbox
