7492db57f76cdeb16a14e30316e868d72ed4e782
Dominik <dominik.kurasbediani@gmail.com>
2023-06-05 09:36:03 +0200

    physical device enumeration

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,148 @@
-extern crate vulkano;
-extern crate vulkano_win;
-extern crate winit;
-
 use std::sync::Arc;
-use vulkano::instance::{ApplicationInfo, Instance, InstanceExtensions, Version};
-
-use winit::{dpi::LogicalSize, Event, EventsLoop, WindowBuilder, WindowEvent};
+use vulkano::{
+    buffer::{Buffer, BufferContents, BufferCreateInfo, BufferUsage},
+    command_buffer::{
+        allocator::StandardCommandBufferAllocator, AutoCommandBufferBuilder, CommandBufferUsage,
+        RenderPassBeginInfo, SubpassContents,
+    },
+    device::{
+        physical::PhysicalDeviceType, Device, DeviceCreateInfo, DeviceExtensions, QueueCreateInfo,
+        QueueFlags,
+    },
+    image::{view::ImageView, ImageAccess, ImageUsage, SwapchainImage},
+    instance::{Instance, InstanceCreateInfo},
+    memory::allocator::{AllocationCreateInfo, MemoryUsage, StandardMemoryAllocator},
+    pipeline::{
+        graphics::{
+            input_assembly::InputAssemblyState,
+            vertex_input::Vertex,
+            viewport::{Viewport, ViewportState},
+        },
+        GraphicsPipeline,
+    },
+    render_pass::{Framebuffer, FramebufferCreateInfo, RenderPass, Subpass},
+    swapchain::{
+        acquire_next_image, AcquireError, Surface, Swapchain, SwapchainCreateInfo,
+        SwapchainCreationError, SwapchainPresentInfo,
+    },
+    sync::{self, FlushError, GpuFuture},
+    VulkanLibrary,
+};
+use vulkano_win::VkSurfaceBuild;
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{self, ControlFlow, EventLoop},
+    window::{Window, WindowBuilder},
+};
 
 const WIDTH: u32 = 800;
 const HEIGHT: u32 = 600;
 
 struct Application {
-    instance: Option<Arc<Instance>>,
-    events_loop: EventsLoop,
+    instance: Arc<Instance>,
 }
 
 impl Application {
-    pub fn init() -> Self {
+    pub fn init() -> (Self, EventLoop<()>) {
         let instance = Self::create_instance();
-        let events_loop = Self::init_window();
-
-        Self {
-            instance,
-            events_loop,
-        }
+        let (events_loop, surface) = Self::init_window(&instance);
+        let device_extensions = DeviceExtensions {
+            khr_swapchain: true,
+            ..DeviceExtensions::empty()
+        };
+        let (physical_device, queue_family_index) = instance
+            .enumerate_physical_devices()
+            .unwrap()
+            .filter(|p| {
+                // Some devices may not support the extensions or features that your application, or
+                // report properties and limits that are not sufficient for your application. These
+                // should be filtered out here.
+                p.supported_extensions().contains(&device_extensions)
+            })
+            .filter_map(|p| {
+                // For each physical device, we try to find a suitable queue family that will execute
+                // our draw commands.
+                //
+                // Devices can provide multiple queues to run commands in parallel (for example a draw
+                // queue and a compute queue), similar to CPU threads. This is something you have to
+                // have to manage manually in Vulkan. Queues of the same type belong to the same queue
+                // family.
+                //
+                // Here, we look for a single queue family that is suitable for our purposes. In a
+                // real-world application, you may want to use a separate dedicated transfer queue to
+                // handle data transfers in parallel with graphics operations. You may also need a
+                // separate queue for compute operations, if your application uses those.
+                p.queue_family_properties()
+                    .iter()
+                    .enumerate()
+                    .position(|(i, q)| {
+                        // We select a queue family that supports graphics operations. When drawing to
+                        // a window surface, as we do in this example, we also need to check that
+                        // queues in this queue family are capable of presenting images to the surface.
+                        q.queue_flags.intersects(QueueFlags::GRAPHICS)
+                            && p.surface_support(i as u32, &surface).unwrap_or(false)
+                    })
+                    // The code here searches for the first queue family that is suitable. If none is
+                    // found, `None` is returned to `filter_map`, which disqualifies this physical
+                    // device.
+                    .map(|i| (p, i as u32))
+            })
+            // All the physical devices that pass the filters above are suitable for the application.
+            // However, not every device is equal, some are preferred over others. Now, we assign each
+            // physical device a score, and pick the device with the lowest ("best") score.
+            //
+            // In this example, we simply select the best-scoring device to use in the application.
+            // In a real-world setting, you may want to use the best-scoring device only as a "default"
+            // or "recommended" device, and let the user choose the device themself.
+            .min_by_key(|(p, _)| {
+                // We assign a lower score to device types that are likely to be faster/better.
+                match p.properties().device_type {
+                    PhysicalDeviceType::DiscreteGpu => 0,
+                    PhysicalDeviceType::IntegratedGpu => 1,
+                    PhysicalDeviceType::VirtualGpu => 2,
+                    PhysicalDeviceType::Cpu => 3,
+                    PhysicalDeviceType::Other => 4,
+                    _ => 5,
+                }
+            })
+            .expect("no suitable physical device found");
+        println!(
+            "Using device: {} (type: {:?})",
+            physical_device.properties().device_name,
+            physical_device.properties().device_type,
+        );
+        (Self { instance }, events_loop)
     }
 
     fn create_instance() -> Arc<Instance> {
-        let supported_extensions = InstanceExtensions::supported_by_core()
-            .expect("failed to retrieve supported extensions");
-        println!("Supported extensions: {:?}", supported_extensions);
-
-        let app_info = ApplicationInfo {
-            application_name: Some("Hello Triangle".into()),
-            application_version: Some(Version {
-                major: 1,
-                minor: 0,
-                patch: 0,
-            }),
-            engine_name: Some("No Engine".into()),
-            engine_version: Some(Version {
-                major: 1,
-                minor: 0,
-                patch: 0,
-            }),
-        };
-
-        let required_extensions = vulkano_win::required_extensions();
-        Instance::new(Some(&app_info), &required_extensions, None)
-            .expect("failed to create Vulkan instance")
+        let library = VulkanLibrary::new().unwrap();
+        let required_extensions = vulkano_win::required_extensions(&library);
+        let instance = Instance::new(
+            library,
+            InstanceCreateInfo {
+                enabled_extensions: required_extensions,
+                // Enable enumerating devices that use non-conformant Vulkan implementations. (e.g.
+                // MoltenVK)
+                enumerate_portability: true,
+                ..Default::default()
+            },
+        )
+        .unwrap();
+        instance
     }
 
-    fn init_window() -> EventsLoop {
-        let events_loop = EventsLoop::new();
-        let _window = WindowBuilder::new()
-            .with_title("Vulkan")
-            .with_dimensions(LogicalSize::new(f64::from(WIDTH), f64::from(HEIGHT)))
-            .build(&events_loop);
-        events_loop
+    fn init_window(instance: &Arc<Instance>) -> (EventLoop<()>, Arc<Surface>) {
+        let event_loop = EventLoop::new();
+        let surface = WindowBuilder::new()
+            .build_vk_surface(&event_loop, instance.clone())
+            .unwrap();
+        (event_loop, surface)
     }
 
-    fn main_loop(&mut self) {
+    fn main_loop(&mut self, events_loop: EventLoop<()>) {
         loop {
             let mut done = false;
-            self.events_loop.poll_events(|ev| {
+            events_loop.run(move |ev, _, control_flow| {
                 if let Event::WindowEvent {
                     event: WindowEvent::CloseRequested,
                     ..
@@ -72,14 +151,11 @@ impl Application {
                     done = true
                 }
             });
-            if done {
-                return;
-            }
         }
     }
 }
 
 fn main() {
-    let mut app = Application::init();
-    app.main_loop();
+    let (mut app, events_loop) = Application::init();
+    app.main_loop(events_loop);
 }
