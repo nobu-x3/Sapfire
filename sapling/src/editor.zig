3a832d39178c3b9c6ce7a80f3cc2b330b01a27a5
Dominik <dominik.kurasbediani@gmail.com>
2023-07-12 22:48:23 +0200

    Added style to editor

--- sapling/src/editor.zig
+++ sapling/src/editor.zig
@@ -33,6 +33,7 @@ pub const Editor = struct {
             @enumToInt(zgpu.GraphicsContext.swapchain_format),
             .{ .texture_filter_mode = .linear, .pipeline_multisample_count = 1 },
         );
+        set_style();
         return Editor{
             .window = window,
             .gctx = gctx,
@@ -91,5 +92,61 @@ pub const Editor = struct {
         AssetManager.deinit();
         log.deinit();
         JobsManager.deinit();
+    }
+
+    fn set_style() void {
+        const style = zgui.getStyle();
+        style.window_min_size = .{ 160.0, 20.0 };
+        style.frame_padding = .{ 4.0, 2.0 };
+        style.item_spacing = .{ 6.0, 2.0 };
+        style.item_inner_spacing = .{ 6.0, 4.0 };
+        style.alpha = 0.95;
+        style.window_rounding = 4.0;
+        style.frame_rounding = 2.0;
+        style.indent_spacing = 6.0;
+        style.item_inner_spacing = .{ 2.0, 4.0 };
+        style.columns_min_spacing = 50.0;
+        style.grab_min_size = 14.0;
+        style.grab_rounding = 16.0;
+        style.scrollbar_size = 12.0;
+        style.scrollbar_rounding = 16.0;
+        style.setColor(zgui.StyleCol.text, .{ 0.86, 0.93, 0.89, 0.78 });
+        style.setColor(zgui.StyleCol.text_disabled, .{ 0.86, 0.93, 0.89, 0.28 });
+        style.setColor(zgui.StyleCol.window_bg, .{ 0.13, 0.14, 0.17, 1.0 });
+        style.setColor(zgui.StyleCol.border, .{ 0.31, 0.31, 1.0, 0.0 });
+        style.setColor(zgui.StyleCol.border_shadow, .{ 0.0, 0.0, 0.0, 0.0 });
+        style.setColor(zgui.StyleCol.frame_bg, .{ 0.2, 0.22, 0.27, 1.0 });
+        style.setColor(zgui.StyleCol.frame_bg_hovered, .{ 0.92, 0.18, 0.29, 0.78 });
+        style.setColor(zgui.StyleCol.frame_bg_active, .{ 0.92, 0.18, 0.29, 1.0 });
+        style.setColor(zgui.StyleCol.title_bg, .{ 0.2, 0.22, 0.27, 1.0 });
+        style.setColor(zgui.StyleCol.title_bg_collapsed, .{ 0.2, 0.22, 0.27, 0.78 });
+        style.setColor(zgui.StyleCol.title_bg_active, .{ 0.92, 0.18, 0.29, 1.0 });
+        style.setColor(zgui.StyleCol.menu_bar_bg, .{ 0.2, 0.22, 0.27, 0.47 });
+        style.setColor(zgui.StyleCol.scrollbar_bg, .{ 0.2, 0.22, 0.27, 1.0 });
+        style.setColor(zgui.StyleCol.scrollbar_grab, .{ 0.09, 0.15, 0.16, 1.0 });
+        style.setColor(zgui.StyleCol.scrollbar_grab_hovered, .{ 0.92, 0.18, 0.29, 0.78 });
+        style.setColor(zgui.StyleCol.scrollbar_grab_active, .{ 0.92, 0.18, 0.29, 1.0 });
+        style.setColor(zgui.StyleCol.check_mark, .{ 0.71, 0.22, 0.27, 1.0 });
+        style.setColor(zgui.StyleCol.slider_grab, .{ 0.47, 0.77, 0.83, 0.14 });
+        style.setColor(zgui.StyleCol.slider_grab_active, .{ 0.92, 0.18, 0.29, 1.0 });
+        style.setColor(zgui.StyleCol.button, .{ 0.47, 0.77, 0.83, 0.14 });
+        style.setColor(zgui.StyleCol.button_hovered, .{ 0.92, 0.18, 0.29, 0.86 });
+        style.setColor(zgui.StyleCol.button_active, .{ 0.92, 0.18, 0.29, 1.0 });
+        style.setColor(zgui.StyleCol.header, .{ 0.92, 0.18, 0.29, 0.76 });
+        style.setColor(zgui.StyleCol.header_hovered, .{ 0.92, 0.18, 0.29, 0.86 });
+        style.setColor(zgui.StyleCol.header_active, .{ 0.92, 0.18, 0.29, 1.0 });
+        style.setColor(zgui.StyleCol.separator, .{ 0.14, 0.16, 0.19, 1.0 });
+        style.setColor(zgui.StyleCol.separator_hovered, .{ 0.92, 0.18, 0.29, 0.78 });
+        style.setColor(zgui.StyleCol.separator_active, .{ 0.92, 0.18, 0.29, 1.0 });
+        style.setColor(zgui.StyleCol.resize_grip, .{ 0.47, 0.77, 0.83, 0.04 });
+        style.setColor(zgui.StyleCol.resize_grip_hovered, .{ 0.92, 0.18, 0.29, 0.78 });
+        style.setColor(zgui.StyleCol.resize_grip_active, .{ 0.92, 0.18, 0.29, 1.0 });
+        style.setColor(zgui.StyleCol.plot_lines, .{ 0.86, 0.93, 0.89, 0.63 });
+        style.setColor(zgui.StyleCol.plot_lines_hovered, .{ 0.92, 0.18, 0.29, 1.0 });
+        style.setColor(zgui.StyleCol.plot_histogram, .{ 0.86, 0.93, 0.89, 0.63 });
+        style.setColor(zgui.StyleCol.plot_histogram_hovered, .{ 0.92, 0.18, 0.29, 1.0 });
+        style.setColor(zgui.StyleCol.text_selected_bg, .{ 0.92, 0.18, 0.29, 0.43 });
+        style.setColor(zgui.StyleCol.popup_bg, .{ 0.2, 0.22, 0.27, 0.9 });
+        style.setColor(zgui.StyleCol.modal_window_dim_bg, .{ 0.2, 0.22, 0.27, 0.73 });
     }
 };
