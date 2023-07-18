b5c580e87fac7dc1b30ea30fc4fd6c2bd04887da
Dominik <dominik.kurasbediani@gmail.com>
2023-07-19 01:00:14 +0200

    Added a different solution for component serialization

--- sapfire/src/scene/scene.zig
+++ sapfire/src/scene/scene.zig
@@ -18,11 +18,11 @@ pub const Scene = struct {
 
     pub fn create(allocator: std.mem.Allocator, gctx: *zgpu.GraphicsContext) !Scene {
         var arena = std.heap.ArenaAllocator.init(allocator);
-        var world = World.init();
+        var world = World.init(allocator);
         _ = gctx;
-        world.component_add(Transform);
-        world.component_add(Position);
-        world.component_add(Mesh);
+        try world.component_add(Transform);
+        try world.component_add(Position);
+        try world.component_add(Mesh);
         world.tag_add(TestTag);
         // {
         //     var sys_desc = ecs.system_desc_t{};
@@ -34,81 +34,9 @@ pub const Scene = struct {
         var first_entt = world.entity_new_with_parent(scene_entity, "Child");
         _ = ecs.add_id(world.id, first_entt, ecs.id(TestTag));
         _ = world.entity_new_with_parent(first_entt, "Grandchild");
+        try world.serialize();
         // const json_world = ecs.world_to_json(world.id, &.{}).?;
         // std.debug.print("\n{s}", .{json_world});
-        var filter_desc = ecs.filter_desc_t{};
-        filter_desc.terms[0] = .{ .id = ecs.Any };
-        const filter = try ecs.filter_init(world.id, &filter_desc);
-        var it = ecs.filter_iter(world.id, filter);
-        while (ecs.filter_next(&it)) {
-            const world_id = it.world;
-            const entities = it.entities();
-            // const transforms = ecs.field(&it, Transform, 1).?;
-            for (entities) |e| {
-                var wrapped_world = World.wrap(world_id);
-                if (!wrapped_world.entity_is_scene_entity(e)) continue;
-                std.debug.print("Entity ID: {d}\n", .{e});
-                const _name = ecs.get_name(world_id, e).?;
-                std.debug.print("\tName: {s}\n", .{_name});
-                const path = wrapped_world.entity_full_path(e, 0);
-                std.debug.print("\tPath: {s}\n", .{path});
-                // std.debug.print("\tTransform: {d}\n", .{transforms[i].matrix});
-                components_stage: {
-                    const types = ecs.get_type(world_id, e).?;
-                    var comp_len: usize = 0;
-                    const type_count = @intCast(usize, types.count);
-                    var components = types.array;
-                    for (types.array[0..type_count]) |comp| {
-                        if (ecs.id_is_pair(comp) or ecs.id_is_tag(world_id, comp)) {
-                            continue;
-                        }
-                        components[comp_len] = comp;
-                        comp_len += 1;
-                    }
-
-                    const component_types: ?*const ecs.type_t = &.{
-                        .array = components,
-                        .count = @intCast(i32, comp_len),
-                    };
-
-                    transform_comp: {
-                        const transform = ecs.get(world_id, e, Transform) orelse break :transform_comp;
-                        std.debug.print("\n{d}\n", .{transform.matrix});
-                    }
-
-                    const str_comps = ecs.type_str(world_id, component_types) orelse break :components_stage;
-                    const casted_comps = std.mem.span(str_comps);
-                    std.debug.print("\tComponents: {s}\n", .{casted_comps});
-                }
-                tags_stage: {
-                    const types = ecs.get_type(world_id, e).?;
-                    var tag_len: usize = 0;
-                    const type_count = @intCast(usize, types.count);
-                    var tags = types.array;
-                    for (types.array[0..type_count]) |comp| {
-                        if (ecs.id_is_pair(comp)) {
-                            continue;
-                        }
-                        if (ecs.id_is_tag(world_id, comp)) {
-                            tags[tag_len] = comp;
-                            tag_len += 1;
-                        }
-                    }
-                    const tags_types: ?*const ecs.type_t = &.{
-                        .array = tags,
-                        .count = @intCast(i32, tag_len),
-                    };
-                    const str_tags = ecs.type_str(world_id, tags_types) orelse {
-                        // no tags
-                        std.debug.print("\tTags:\n", .{});
-                        break :tags_stage;
-                    };
-                    const casted_tags = std.mem.span(str_tags);
-                    std.debug.print("\tTags: {s}\n", .{casted_tags});
-                }
-            }
-        }
-        std.debug.print("\n", .{});
         // _ = ecs.progress(world.id, 0);
 
         return Scene{
@@ -120,18 +48,20 @@ pub const Scene = struct {
     }
 
     pub fn destroy(self: *Scene, allocator: std.mem.Allocator) void {
-        _ = allocator;
-        self.world.deinit();
+        self.world.deinit(allocator);
     }
 };
 
 pub const World = struct {
     id: *ecs.world_t,
+    component_id_map: std.AutoHashMap(ecs.id_t, [:0]const u8),
 
-    pub fn init() World {
+    pub fn init(allocator: std.mem.Allocator) World {
         const id = ecs.init();
+        const map = std.AutoHashMap(ecs.id_t, [:0]const u8).init(allocator);
         return World{
             .id = id,
+            .component_id_map = map,
         };
     }
 
@@ -141,12 +71,16 @@ pub const World = struct {
         };
     }
 
-    pub fn deinit(self: *World) void {
+    pub fn deinit(self: *World, allocator: std.mem.Allocator) void {
+        allocator.free(self.component_id_map);
         _ = ecs.fini(self.id);
     }
 
-    pub fn component_add(self: *World, comptime T: type) void {
+    pub fn component_add(self: *World, comptime T: type) !void {
         ecs.COMPONENT(self.id, T);
+        const id = ecs.id(T);
+        try self.component_id_map.put(id, @typeName(T));
+        std.debug.print("{s}: ID {d}", .{ @typeName(T), ecs.id(T) });
     }
 
     pub fn tag_add(self: *World, comptime T: type) void {
@@ -209,5 +143,88 @@ pub const World = struct {
             break :val separator_index;
         };
         return path[0..len];
+    }
+
+    pub fn serialize(self: *World) !void {
+        var filter_desc = ecs.filter_desc_t{};
+        filter_desc.terms[0] = .{ .id = ecs.Any };
+        const filter = try ecs.filter_init(self.id, &filter_desc);
+        var it = ecs.filter_iter(self.id, filter);
+        while (ecs.filter_next(&it)) {
+            const world_id = it.world;
+            const entities = it.entities();
+            // const transforms = ecs.field(&it, Transform, 1).?;
+            for (entities) |e| {
+                if (!self.entity_is_scene_entity(e)) continue;
+                std.debug.print("Entity ID: {d}\n", .{e});
+                const _name = ecs.get_name(world_id, e).?;
+                std.debug.print("\tName: {s}\n", .{_name});
+                const path = self.entity_full_path(e, 0);
+                std.debug.print("\tPath: {s}\n", .{path});
+                // std.debug.print("\tTransform: {d}\n", .{transforms[i].matrix});
+                components_stage: {
+                    const types = ecs.get_type(world_id, e).?;
+                    var comp_len: usize = 0;
+                    const type_count = @intCast(usize, types.count);
+                    var components = types.array;
+                    for (types.array[0..type_count]) |comp| {
+                        if (ecs.id_is_pair(comp) or ecs.id_is_tag(world_id, comp)) {
+                            continue;
+                        }
+                        components[comp_len] = comp;
+                        comp_len += 1;
+                    }
+
+                    const component_types: ?*const ecs.type_t = &.{
+                        .array = components,
+                        .count = @intCast(i32, comp_len),
+                    };
+
+                    for (components, 0..comp_len) |comp, _| {
+                        if (self.component_id_map.contains(comp)) {
+                            const comp_name = self.component_id_map.get(comp).?;
+                            if (std.mem.eql(u8, comp_name, "scene.components.Transform")) { // TODO: think of a better way of doing this
+                                const transform = ecs.get(world_id, e, Transform).?;
+                                std.debug.print("\n{d}\n", .{transform.matrix});
+                            } else if (std.mem.eql(u8, comp_name, "scene.components.Position")) {
+                                const pos = ecs.get(world_id, e, Position).?;
+                                std.debug.print("\n{d} {d} {d}\n", .{ pos.x, pos.y, pos.z });
+                            }
+                        }
+                    }
+
+                    const str_comps = ecs.type_str(world_id, component_types) orelse break :components_stage;
+                    const casted_comps = std.mem.span(str_comps);
+                    std.debug.print("\tComponents: {s}\n", .{casted_comps});
+                }
+                tags_stage: {
+                    const types = ecs.get_type(world_id, e).?;
+                    var tag_len: usize = 0;
+                    const type_count = @intCast(usize, types.count);
+                    var tags = types.array;
+                    for (types.array[0..type_count]) |comp| {
+                        if (ecs.id_is_pair(comp)) {
+                            continue;
+                        }
+                        if (ecs.id_is_tag(world_id, comp)) {
+                            tags[tag_len] = comp;
+                            tag_len += 1;
+                        }
+                    }
+                    const tags_types: ?*const ecs.type_t = &.{
+                        .array = tags,
+                        .count = @intCast(i32, tag_len),
+                    };
+                    const str_tags = ecs.type_str(world_id, tags_types) orelse {
+                        // no tags
+                        std.debug.print("\tTags:\n", .{});
+                        break :tags_stage;
+                    };
+                    const casted_tags = std.mem.span(str_tags);
+                    std.debug.print("\tTags: {s}\n", .{casted_tags});
+                }
+            }
+        }
+        std.debug.print("\n", .{});
     }
 };
