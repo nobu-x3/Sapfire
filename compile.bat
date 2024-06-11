3cdcca6aa5eb10ed69bfe69406f6465b92803452
Dominik <dominik.kurasbediani@gmail.com>
2024-06-11 22:19:16 +0100

    Fixed build scripts and repo to make it compile on devices that do not have the required deps

--- compile.bat
+++ compile.bat
@@ -1,4 +1,4 @@
-cmake -B build -DCMAKE_BUILD_TYPE=Debug -G Ninja -DCMAKE_CXX_COMPILER="C:/Program Files/LLVM/bin/clang++.exe" -DCMAKE_C_COMPILER="C:/Program Files/LLVM/bin/clang.exe" -Wdeprecated-declarations -DROOT_PATH_ABS=%cd% -DQT_DIR:PATH=C:/Qt/6.5.3/msvc2019_64/lib/cmake/Qt6 -DQT_QMAKE_EXECUTABLE:FILEPATH=C:/Qt/6.5.3/msvc2019_64/bin/qmake.exe -DQt6CoreTools_DIR:PATH=C:/Qt/6.5.3/msvc2019_64/lib/cmake/Qt6CoreTools -DQt6Core_DIR:PATH=C:/Qt/6.5.3/msvc2019_64/lib/cmake/Qt6Core -DQt6EntryPointPrivate_DIR:PATH=C:/Qt/6.5.3/msvc2019_64/lib/cmake/Qt6EntryPointPrivate -DQt6GuiTools_DIR:PATH=C:/Qt/6.5.3/msvc2019_64/lib/cmake/Qt6GuiTools -DQt6Gui_DIR:PATH=C:/Qt/6.5.3/msvc2019_64/lib/cmake/Qt6Gui -DQt6WidgetsTools_DIR:PATH=C:/Qt/6.5.3/msvc2019_64/lib/cmake/Qt6WidgetsTools -DQt6Widgets_DIR:PATH=C:/Qt/6.5.3/msvc2019_64/lib/cmake/Qt6Widgets -DQt6ZlibPrivate_DIR:PATH=C:/Qt/6.5.3/msvc2019_64/lib/cmake/Qt6ZlibPrivate -DQt6_DIR:PATH=C:/Qt/6.5.3/msvc2019_64/lib/cmake/Qt6
+cmake -B build -DCMAKE_BUILD_TYPE=Debug -G Ninja -DCMAKE_CXX_COMPILER="C:/Program Files/LLVM/bin/clang++.exe" -DCMAKE_C_COMPILER="C:/Program Files/LLVM/bin/clang.exe" -Wdeprecated-declarations -DROOT_PATH_ABS=%cd% -DDIRECTX_DXC_TOOL=%cd%/engine/vendor/dxc/_bin/x64/dxc.exe
 cd build
 :: ninja Shaders
 ninja
@@ -11,10 +11,11 @@ xcopy /S /I /H /Q /Y build\engine\vendor\*.lib bin
 xcopy /S /I /H /Q /Y build\engine\vendor\*.exe bin
 xcopy /S /I /H /Q /Y build\assets bin\assets
 xcopy /S /I /H /Q /Y editor_assets bin\editor_assets
-::xcopy /S /I /H /Q /Y build\vendor\dxc\lib\x64\dxcompiler.lib bin
-::xcopy /S /I /H /Q /Y vendor\dxc\lib\x64\dxcompiler.pdb bin
-xcopy /S /I /H /Q /Y engine\vendor\dxc\bin\x64\dxil.dll bin
-xcopy /S /I /H /Q /Y engine\vendor\dxc\bin\x64\dxil.pdb bin
+xcopy /S /I /H /Q /Y build\vendor\dxc\lib\x64\dxcompiler.lib bin
+xcopy /S /I /H /Q /Y build\vendor\dxc\lib\x64\dxcompiler.pdb bin
+xcopy /S /I /H /Q /Y build\engine\vendor\dxc\lib\x64\dxcompiler.dll bin
+xcopy /S /I /H /Q /Y engine\vendor\dxc\_bin\x64\dxil.dll bin
+xcopy /S /I /H /Q /Y engine\vendor\dxc\_bin\x64\dxil.pdb bin
 :: xcopy /S /I /H /Q /Y build\vendor\DirectXTK12\Bin\Windows10_2022\x64\Release\* bin
 :: xcopy /S /I /H /Q /Y SaplingQt\vendor\Qt6\lib\*.dll bin
 :: xcopy /S /I /H /Q /Y SaplingQt\vendor\Qt6\lib\*.lib bin
