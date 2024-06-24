482d82b957ae40cf38eeeb748dadc5f5c486a331
Dominik <dominik.kurasbediani@gmail.com>
2024-06-24 15:41:05 +0200

    Subeditors now have their own dockspace

--- sapling/src/subeditors/subeditor.cpp
+++ sapling/src/subeditors/subeditor.cpp
@@ -8,7 +8,10 @@ using namespace Sapfire;
 SSubeditor::SSubeditor(Sapfire::stl::string_view editor_name) : m_Name(editor_name) {}
 
 bool SSubeditor::update(f32 delta_time) {
-	ImGui::DockSpaceOverViewport(ImGui::GetMainViewport());
+	//ImGui::DockSpaceOverViewport(ImGui::GetMainViewport());
+	ImGui::Begin(m_Name.c_str(), nullptr);
+	ImGuiID dockspace_id = ImGui::GetID("###SubeditorDockspace");
+	ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f));
 	ImGuiIO io = ImGui::GetIO();
 	for (int i = 0; i < IM_ARRAYSIZE(io.MouseDown); i++) {
 		if (ImGui::IsMouseClicked(i)) {
@@ -76,6 +79,7 @@ bool SSubeditor::update(f32 delta_time) {
 	for (auto& widget : m_Widgets) {
 		return_val &= widget->update(delta_time);
 	}
+	ImGui::End();
     return return_val;
 }
 
